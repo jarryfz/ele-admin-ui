2751e3b759377d87e88d793298aec16b43e0d409
lzr <laizerongfz@163.com>
2023-08-25 22:04:23 +0800

    下拉多选框

--- src/components/ele-select-tree/index.vue
+++ src/components/ele-select-tree/index.vue
@@ -16,6 +16,7 @@
           :props="defaultProps"
           v-bind="$attrs.props"
           @node-click="handleNodeClick"
+          @check-change="handleCheckChange"
         >
         </el-tree>
       </el-option>
@@ -27,10 +28,10 @@ export default {
   name: "EleSelectTree",
   inheritAttrs: false,
   props: {
-    value: {
-      type: [String, Number],
-      default: ""
-    },
+    // value: {
+    //   type: [String, Number],
+    //   default: ""
+    // },
     defaultProps: {
       type: Object,
       default: () => ({
@@ -41,6 +42,7 @@ export default {
   },
   data() {
     return {
+      value: "",
       optionData: {
         label: "",
         value: ""
@@ -56,6 +58,13 @@ export default {
       this.$emit("input", data[this.$attrs.nodeKey || "id"])
       this.$refs.eleSelectTreeRef.visible = false
     },
+    handleCheckChange(data, checked, childChecked) {
+      console.log(data, checked, childChecked)
+      const checkeNodes = this.$refs.eleTreeRef.getCheckedNodes(),
+       checkedKeys = this.$refs.eleTreeRef.getCheckedKeys();
+      this.value = checkeNodes.map(item => item.label);
+      this.$emit("checkChange", checkeNodes, checkedKeys);
+    }
   }
 }
 </script>
